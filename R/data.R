45313ee5e64827f995bfcfd6d8f46c86b6fb7d7b
Marc Choisy <marc.choisy@ird.fr>
2019-08-21 15:54:30 +0700

    Adding srtm_prov data

--- R/data.R
+++ R/data.R
@@ -15,3 +15,33 @@
 #' ## Extracting the elevation in one location:
 #' ## raster::extract
 "srtm90"
+
+#' SRTM averages by province
+#'
+#' Average elevations by province, weighted or not by local population density.
+#'
+#' Local population density data are coming from the WorldPop project.
+#'
+#' @author Marc Choisy
+#' @examples
+#' \dontrun{
+#' # loading the data:
+#' library(srtmVN)
+#' data(srtm_prov)
+#'
+#' # comparing the 2 measures of aggregated elevation:
+#' plot(elevation1 ~ elevation2, srtm_prov, xlab = "weighted by population density", ylab = "non weighted")
+#' abline(0, 1)
+#'
+#' # mapping the 2 measures:
+#' library(sf)
+#' provinces <- gadmVN::gadm()
+#' provinces <- merge(provinces, srtm_prov)
+#' plot(provinces[c("elevation1", "elevation2")])
+#'
+#' # mapping the differences between the 2 measures:
+#' provinces$elevation_diff <- with(srtm_prov, elevation1 - elevation2)
+#' plot(provinces["elevation_diff"])
+#' }
+#'
+"srtm_prov"
